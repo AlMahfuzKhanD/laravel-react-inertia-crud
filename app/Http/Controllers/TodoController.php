c29ee577b790f2e54cdd8af4c22c25fb8f24547e
mahfuz380 <almahfuz380@gmail.com>
2021-05-18 13:03:52 +0600

    custom validation message added

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Todo;
-use Dotenv\Validator;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
@@ -27,26 +27,26 @@ class TodoController extends Controller
     public function store(Request $request)
     {
 
-        $request->validate([
-            'title' => 'required | max:255 | min:4'
-        ]);
-        // $rules = [
+        // $request->validate([
         //     'title' => 'required | max:255 | min:4'
-        // ];
+        // ]);
+        $rules = [
+            'title' => 'required | max:255 | min:4'
+        ];
 
-        // $messages = [
-        //     'title.required' => 'title is required.',
-        //     'title.max' => 'Max 255 character approved',
-        //     'title.min' => 'Min 4 character approved',
-        // ];
+        $messages = [
+            'title.required' => 'title is required.',
+            'title.max' => 'Max 255 character approved',
+            'title.min' => 'Min 4 character approved'
+        ];
         
-        // $validator = Validator::make($request->all(), $rules, $messages);
+        $validator = Validator::make($request->all(), $rules, $messages);
 
-        // if ($validator->fails()) {
-        //     return redirect()->back()
-        //                 ->withErrors($validator)
-        //                 ->withInput();
-        // }
+        if ($validator->fails()) {
+            return redirect()->back()
+                        ->withErrors($validator)
+                        ->withInput();
+        }
 
         Todo::create($request->all());
         return redirect()->back()->with('message', 'Todo Created Successfully');
