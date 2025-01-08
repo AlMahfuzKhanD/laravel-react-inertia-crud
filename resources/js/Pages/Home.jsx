0272eaceea42350c8bc9e684bc34a96cfb525758
AlMahfuzKhanD <almahfuz380@gmail.com>
2025-01-08 11:27:00 +0600

    progress indicator inertian practice done

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,9 +1,9 @@
 import { Link } from "@inertiajs/react";
 
-export default function Home() {
+export default function Home({ name }) {
     return (
         <>
-            <h1 className="title">Hello user</h1>
+            <h1 className="title">Hello {name}</h1>
             <Link preserveScroll href="/" className="block title mt-[1000px]">
                 {new Date().toLocaleTimeString()}
             </Link>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
+use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return inertia('Home');
 // });
 
-Route::inertia('/', 'Home');
+// Route::inertia('/', 'Home');
+
+Route::get('/', function(){
+    sleep(2);
+    return Inertia::render('Home', ['name' => 'Mahfuz']);
+});
