a90bfa4d8c8fe1454b2b0654bac5e4179d636c10
AlMahfuzKhanD <almahfuz380@gmail.com>
2025-01-08 11:55:50 +0600

    Model prepare done

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 // Route::get('/', function () {
 //     return inertia('Home');
@@ -9,7 +10,10 @@ use Illuminate\Support\Facades\Route;
 
 // Route::inertia('/', 'Home');
 
-Route::get('/', function(){
-    sleep(2);
-    return Inertia::render('Home', ['name' => 'Mahfuz']);
-});
+// Route::get('/', function(){
+//     sleep(2);
+//     return Inertia::render('Home', ['name' => 'Mahfuz']);
+// });
+
+Route::get('/',[PostController::class,'index']);
+Route::resource('posts', PostController::class)->except('index');
